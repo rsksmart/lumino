6164a3b48438bbbb2112e789390fec3473a2c3ee
Gaspar Medina <gmedina@infuy.com>
2019-06-13 16:24:12 -0300

    [FIX] alive signal and register node
    Alive signal and register node now have mandatory params

--- raiden/schedulers/alive_schedule.py
+++ raiden/schedulers/alive_schedule.py
@@ -13,20 +13,26 @@ def notice_explorer_to_be_alive(endpoint_explorer, node_address):
     """
     try:
 
-        payload = {
-            'last_alive_signal': datetime.utcnow().isoformat()
-        }
-
-        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-
         url = endpoint_explorer + "luminoNode/" + node_address
 
-        response = requests.put(url, data=json.dumps(payload), headers=headers)
+        response = requests.get(url)
 
         if response.status_code == 200:
-            log.info("Succesfully send alive signal to Lumino Explorer")
+            json_data = json.loads(response.text)
+            data = json_data['data']
+            data['last_alive_signal'] = datetime.utcnow().isoformat()
+
+            headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+
+            response = requests.put(url, json=data, headers=headers)
+
+            if response.status_code == 200:
+                log.info("Succesfully send alive signal to Lumino Explorer")
+            else:
+                log.info("Warning: There was an error sending alive signal to Lumino Explorer. Status: " +
+                         str(response.status_code))
         else:
-            log.info("Warning: There was an error sending alive signal to Lumino Explorer. Status: " + str(response.status_code))
+            log.info("Warning: send alive signal to Lumino Explorer, is not posible because node is not registered")
 
     except requests.exceptions.RequestException as e:
         log.info("Warning: Could not connect to Lumino Explorer. Your node will not send alive signal.")
--- raiden/ui/explorer.py
+++ raiden/ui/explorer.py
@@ -1,17 +1,26 @@
 import requests
 import structlog
+import json
+from datetime import datetime
 
 log = structlog.get_logger(__name__)
 
 
 def register(explorer_endpoint, rsk_address, rns_domain):
     try:
-        r = requests.post(explorer_endpoint + 'luminoNode/',
-                          json={'node_address': rsk_address, 'rns_address': rns_domain})
-        if r.status_code == 200:
+
+        if rns_domain is None:
+            rns_domain = ''
+
+        data_registry = {'node_address': rsk_address,
+                         'rns_address': rns_domain,
+                         'last_alive_signal': datetime.utcnow().isoformat()}
+
+        response = requests.post(explorer_endpoint + 'luminoNode/', json=data_registry)
+        if response.status_code == 200:
             log.info("Succesfully registered into Lumino Explorer")
         else:
-            log.info("Warning: There was an error registering into Lumino Explorer. Status: " + str(r.status_code))
+            log.info("Warning: There was an error registering into Lumino Explorer. Status: " + str(response.status_code))
     except requests.exceptions.RequestException as e:
         log.info("Warning: Could not connect to Lumino Explorer. Your node will not be registered.")
 
