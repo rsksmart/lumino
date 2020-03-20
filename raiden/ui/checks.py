c823359107152eefb764aeb7ab0d9e9ff374526f
mmartinez@infuy.com <mmartinez@infuy.com>
2020-03-20 17:40:21 -0300

    Added routing-mode on startup.py

--- raiden/ui/checks.py
+++ raiden/ui/checks.py
@@ -137,27 +137,17 @@ def check_smart_contract_addresses(
 
 
 def check_pfs_configuration(
-    routing_mode: RoutingMode,
-    environment_type: Environment,
-    service_registry: Optional[ServiceRegistry],
-    pathfinding_service_address: str,
+    service_registry: Optional[ServiceRegistry], pathfinding_service_address: str
 ) -> None:
-    if routing_mode == RoutingMode.PFS:
-        if environment_type == Environment.PRODUCTION:
-            click.secho(
-                "Requested production mode and PFS routing mode. This is not supported", fg="red"
-            )
-            sys.exit(1)
-
-        if not service_registry and not pathfinding_service_address:
-            click.secho(
-                "Requested PFS routing mode but no service registry or no specific pathfinding "
-                " service address is provided. Please provide it via either the "
-                "--service-registry-contract-address or the --pathfinding-service-address "
-                "argument",
-                fg="red",
-            )
-            sys.exit(1)
+    if not service_registry and not pathfinding_service_address:
+        click.secho(
+            "Requested PFS routing mode but no service registry or no specific pathfinding "
+            " service address is provided. Please provide it via either the "
+            "--service-registry-contract-address or the --pathfinding-service-address "
+            "argument",
+            fg="red",
+        )
+        sys.exit(1)
 
 
 def check_synced(blockchain_service: BlockChainService) -> None:
