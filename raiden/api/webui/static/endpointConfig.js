6931b8bd40126fe3cc1cf4dd2c0cffdfa789e964
Mauricio Irace <mirace@infuy.com>
2020-08-17 14:26:54 -0300

    Inject storage into mediated transfer

---
+++ raiden/api/webui/static/endpointConfig.js
@@ -0,0 +1,13 @@
+const backendUrl='http://localhost:5001'; 
+const nodeAddress = '0x4693688986f02569E2b4842e4D676C46a86c0388'; 
+const rnsDomain = null 
+const chainEndpoint = 'http://localhost:4444'; 
+
+window.luminoUrl = backendUrl;
+window.nodeAddress= nodeAddress;
+window.rnsDomain = rnsDomain;
+window.chainEndpoint = chainEndpoint;
+
+
+
+
