321cef901edab102bf9b2ba9fcdddba99af89c2f
Mauricio Irace <mirace@infuy.com>
2020-08-19 11:05:07 -0300

    Readded endpoint config

---
+++ raiden/api/webui/static/endpointConfig.js
@@ -0,0 +1,13 @@
+const backendUrl='http://localhost:5002'; 
+const nodeAddress = '0x62Aa1173561C4E92876B3D073B9767A2d07496f8'; 
+const rnsDomain = null 
+const chainEndpoint = 'http://localhost:4444'; 
+
+window.luminoUrl = backendUrl;
+window.nodeAddress= nodeAddress;
+window.rnsDomain = rnsDomain;
+window.chainEndpoint = chainEndpoint;
+
+
+
+
