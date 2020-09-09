2cadbac75267e9500fd92b42b74c45e89556a648
Mauricio Irace <mirace@infuy.com>
2020-09-09 14:42:12 -0300

    Undo changes in endpointConfig.js

--- raiden/api/webui/static/endpointConfig.js
+++ raiden/api/webui/static/endpointConfig.js
@@ -1,13 +1,12 @@
-const backendUrl='http://localhost:5001'; 
-const nodeAddress = '0xd4939Fc92c8E39D30A87Fe674e475D4B20A3F199'; 
-const rnsDomain = null 
-const chainEndpoint = 'http://165.227.110.216:4444'; 
+const backendUrl='http://localhost:5002';
+const nodeAddress = '0x62Aa1173561C4E92876B3D073B9767A2d07496f8';
+const rnsDomain = null
+const chainEndpoint = 'http://localhost:4444';
 
 window.luminoUrl = backendUrl;
 window.nodeAddress= nodeAddress;
 window.rnsDomain = rnsDomain;
 window.chainEndpoint = chainEndpoint;
-
 
 
 
