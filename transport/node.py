1d097a998a84d6538c89b6077d188f5750f0b900
mortelli <ortellimarcelo@gmail.com>
2020-09-21 16:42:41 -0300

    fix whitespace

--- transport/node.py
+++ transport/node.py
@@ -16,7 +16,7 @@ class Node(ABC):
         Messages to be received by this Node should be have this address as the message receiver.
         Messages to be sent from this Node should have this address as the message sender.
         """
-        self._address = address  
+        self._address = address
 
     @property
     def address(self):
