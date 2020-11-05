9d85e858399b7dfdfd1f6fea0565baecd7b9d059
mmartinez@infuy.com <mmartinez@infuy.com>
2020-11-05 19:09:33 -0300

    Iterate over rif comms client and basic test

--- transport/rif_comms/client.py
+++ transport/rif_comms/client.py
@@ -1,12 +1,14 @@
 from eth_typing import Address
+from grpc import insecure_channel
 
 from transport.message import Message
-from transport.rif_comms.proto.api_pb2 import Notification, Void
+from transport.rif_comms.proto.api_pb2 import Notification, Void, PublishPayload, Channel, Msg, RskAddress
+from transport.rif_comms.proto.api_pb2_grpc import CommunicationsApiStub
 
 
 class RifCommsClient:
     """
-    SDK like class to connect and operate against a RIF Communications pub-sub node.
+    Client like class to connect and operate against a RIF Communications pub-sub node.
     """
 
     def __init__(self, node_address: Address, grpc_api_endpoint: str):
@@ -15,16 +17,17 @@ class RifCommsClient:
         :param node_address: address of the node that wants to use the RIF Comms server
         :param grpc_api_endpoint: http uri of the RIF Communications pub-sub node
         """
-        self.node_address = node_address
+        self.node_address = RskAddress(address=node_address)
+        self.grpc_channel = insecure_channel(grpc_api_endpoint)
+        self.stub = CommunicationsApiStub(self.grpc_channel)
 
-    def connect(self, node_address: Address) -> Notification:
+    def connect(self) -> Notification:
         """
         Connects to RIF Communications Node.
         Invokes ConnectToCommunicationsNode grpc api endpoint.
-        :param node_address:  address of the client node that is trying to connect.
         :return: Notification stream
         """
-        raise NotImplementedError
+        return self.stub.ConnectToCommunicationsNode(self.node_address)
 
     def create_topic(self, partner_address: Address) -> Notification:
         """
@@ -33,17 +36,25 @@ class RifCommsClient:
         :param partner_address:
         :return: Notification stream
         """
-        raise NotImplementedError
+        # TODO catch already subscribed and any error
+        return self.stub.CreateTopicWithRskAddress(RskAddress(address=partner_address))
 
-    def send_message(self, receiver: Address, message: Message) -> Void:
+    def send_message(self, topic_id: Address, message: Message) -> Void:
         """
         Sends a message to receiver node.
         Invokes the SendMessageToTopic grpc api endpoint
-        :param receiver: rsk address of the receiver node
+        :param topic_id: topic identifier
         :param message: the message data
         :return: void
         """
-        raise NotImplementedError
+
+        # TODO message encoding
+        self.stub.SendMessageToTopic(
+            PublishPayload(
+                topic=Channel(channelId=topic_id),
+                message=Msg(payload=str.encode("Test message"))
+            )
+        )
 
     def close_topic(self, topic_id: str) -> Void:
         """
@@ -52,7 +63,7 @@ class RifCommsClient:
         :param topic_id: topic identifier
         :return: void
         """
-        raise NotImplementedError
+        self.stub.CloseTopic(Channel(channelId=topic_id))
 
     def disconnect(self) -> Void:
         """
@@ -60,4 +71,12 @@ class RifCommsClient:
         Invokes the EndCommunication grpc api endpoint.
         :return: void
         """
-        raise NotImplementedError
+        self.grpc_channel.unsubscribe()
+
+    def locate_peer_id(self, node_address: Address) -> str:
+        """
+        Gets the peer id associated with a node address
+        :param node_address: the node address to locate
+        :return: a string that represents the peer id
+        """
+        return self.stub.LocatePeerId(RskAddress(address=node_address)).address
