c4e7013304a1196dc24a91ea44e1eb46b4af30f7
Mauricio Irace <mirace@infuy.com>
2020-12-02 14:07:52 -0300

    Remove unused imports

--- raiden/raiden_event_handler.py
+++ raiden/raiden_event_handler.py
@@ -17,7 +17,6 @@ from raiden.messages import RequestRegisterSecret, UnlockLightRequest, Settlemen
 from raiden.network.proxies.payment_channel import PaymentChannel
 from raiden.network.proxies.token_network import TokenNetwork
 from raiden.network.resolver.client import reveal_secret_with_resolver
-from raiden.storage.restore import channel_state_until_state_change
 from raiden.transfer import views
 from raiden.transfer.architecture import Event
 from raiden.transfer.balance_proof import pack_balance_proof_update
@@ -64,8 +63,6 @@ from raiden.transfer.unlock import get_channel_state, should_search_events, shou
 from raiden.transfer.utils import (
     get_event_with_balance_proof_by_balance_hash,
     get_state_change_with_balance_proof_by_balance_hash,
-    get_state_change_with_balance_proof_by_locksroot,
-    get_event_with_balance_proof_by_locksroot,
 )
 from raiden.transfer.views import get_channelstate_by_token_network_and_partner
 from raiden.utils import pex
