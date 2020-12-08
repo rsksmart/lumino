0fd95dcf5f2d5bc1f9e32f53bcc8e6ed0efd6d8a
mortelli <ortellimarcelo@gmail.com>
2020-12-08 15:51:06 -0300

    iterate configuration loading in rif comms client integration tests

--- raiden/tests/integration/network/transport/rif_comms/node.py
+++ raiden/tests/integration/network/transport/rif_comms/node.py
@@ -3,6 +3,7 @@ import signal
 import subprocess
 import time
 
+import json5
 import psutil
 
 from raiden.tests.integration.network.transport.utils import generate_address
@@ -11,11 +12,30 @@ from transport.rif_comms.client import Client as RIFCommsClient
 connections = {}  # hack to get around the fact that each connect() call needs to be assigned
 
 
+class Config:
+    comms_path = r"/home/rafa/repos/github/rsksmart/rif-communications-pubsub-node"
+    api_endpoint_prefix = "localhost"
+    env_file_prefix = "testing_"
+
+    def __init__(self, node_number: int):
+        # TODO: generate these files
+        self.env_name = self.env_file_prefix + str(node_number)
+        self.env_file = self.comms_path + '/config/' + self.env_name + '.json5'
+
+        # read config file
+        with open(self.env_file, 'r') as reader:
+            config = json5.loads(reader.read())
+            self.listening_port = config['grpcPort']
+
+        self.address = generate_address()
+        self.api_endpoint = self.api_endpoint_prefix + ":" + str(self.listening_port)
+
+
 class Node:
-    def __init__(self, config: 'Config'):
+    def __init__(self, config: Config):
         self.address = config.address
         self.api_endpoint = config.api_endpoint
-        self.env_file = config.env_file
+        self.env_name = config.env_name
 
         self.client = RIFCommsClient(rsk_address=self.address, grpc_api_endpoint=self.api_endpoint)
         self.process = self.start()
@@ -25,8 +45,8 @@ class Node:
         # TODO: look into using shell=False
         # FIXME: write output to memory or disk
         process = subprocess.Popen(
-            "NODE_ENV=" + self.env_file + " npm run api-server",
-            cwd=r"/home/rafa/repos/github/rsksmart/rif-communications-pubsub-node",
+            "NODE_ENV=" + self.env_name + " npm run api-server",
+            cwd=Config.comms_path,
             shell=True,
             preexec_fn=os.setsid,  # set to later kill process group
         )
@@ -49,17 +69,3 @@ class Node:
             for child in psutil.Process(self.process.pid).children(recursive=True):
                 child.kill()
             os.killpg(os.getpgid(self.process.pid), signal.SIGTERM)
-
-
-class Config:
-    starting_port = 5013
-    api_endpoint_prefix = "localhost"
-    env_file_prefix = "development"
-
-    def __init__(self, node_number: int):
-        self.address = generate_address()
-        self.api_endpoint = self.api_endpoint_prefix + ":" + str(
-            self.starting_port + (node_number - 1) * 1000  # 5013, 6013, 7013...
-        )
-        # TODO: generate these files
-        self.env_file = self.env_file_prefix + str(1 + node_number)  # development2, development3...
