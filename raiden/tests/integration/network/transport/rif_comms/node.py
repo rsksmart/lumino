d7a8cc515f0b858e406e8d8bf9a5b36eb57245ea
mortelli <ortellimarcelo@gmail.com>
2020-12-10 10:46:21 -0300

    RLP-919: automate comms tests (#170)
    automate rif comms integration tests

---
+++ raiden/tests/integration/network/transport/rif_comms/node.py
@@ -0,0 +1,67 @@
+from subprocess import Popen
+
+import json5
+
+from raiden.tests.integration.network.transport.rif_comms.process import RIF_COMMS_PATH, Process as CommsProcess
+from raiden.tests.integration.network.transport.utils import generate_address
+from transport.rif_comms.client import Client as RIFCommsClient
+
+
+class Config:
+    """
+    Class to load and set configuration attributes for a RIF Comms node to be run.
+    """
+
+    api_endpoint_prefix = "localhost"
+    env_file_prefix = "testing_"
+
+    def __init__(self, node_number: int):
+        """
+        Load and set a configuration attributes for a RIF Comms node.
+        A valid configuration file must exist at RIF_COMMS_PATH/config/testing_<node_number>.json5
+        """
+        self.env_name = self.env_file_prefix + str(node_number)
+        # TODO: generate these files if needed
+        self._env_file = RIF_COMMS_PATH.joinpath('config/' + self.env_name + '.json5')
+
+        # load config from file
+        with open(self._env_file, 'r') as reader:
+            config = json5.loads(reader.read())
+            self._listening_port = config['grpcPort']
+
+        self.address = generate_address()
+        self.api_endpoint = self.api_endpoint_prefix + ":" + str(self._listening_port)
+
+
+class Node:
+    """
+    Class for RIF Comms node program.
+    """
+    connections = {}  # hack to get around the fact that each connect() call needs to be assigned
+
+    def __init__(self, config: Config):
+        self.address = config.address
+        self._api_endpoint = config.api_endpoint
+        self._env_name = config.env_name
+
+        self.client = RIFCommsClient(rsk_address=self.address, grpc_api_endpoint=self._api_endpoint)
+        self._process = self.start()
+
+    def start(self) -> Popen:
+        """
+        Start a RIF Comms node process and connect to it.
+        """
+        process = CommsProcess.start(env_name=self._env_name)
+        # FIXME: client.connect() calls should not need assignment
+        self.connections[self.address] = self.client.connect()
+        return process
+
+    def stop(self):
+        """
+        Disconnect from RIF Comms node program, and stop its process.
+        """
+        try:
+            # FIXME: deleting entries in the connections dictionary is causing non-crashing thread exceptions
+            self.client.disconnect()
+        finally:
+            CommsProcess.stop(process=self._process)
---
+++ raiden/tests/integration/network/transport/rif_comms/test_client.py
@@ -0,0 +1,123 @@
+import pytest
+
+from raiden.tests.integration.network.transport.rif_comms.node import Node as CommsNode, Config as CommsConfig
+from raiden.tests.integration.network.transport.utils import generate_address
+from transport.rif_comms.utils import notification_to_payload
+
+
+@pytest.fixture()
+@pytest.mark.parametrize("amount_of_nodes")
+def comms_nodes(amount_of_nodes) -> {int, CommsNode}:
+    nodes = {}
+
+    # setup
+    for i in range(1, amount_of_nodes + 1):
+        node = CommsNode(CommsConfig(i))
+        nodes[i] = node
+
+    yield nodes
+
+    # teardown
+    for node in nodes.values():
+        node.stop()
+
+
+@pytest.mark.parametrize("amount_of_nodes", [1])
+def test_locate_own_peer_id(comms_nodes):
+    comms_node = comms_nodes[1]
+
+    # attempt to locate self
+    assert comms_node.client._get_peer_id(comms_node.address) is not ""
+
+
+# FIXME: causes ERR_NO_PEERS_IN_ROUTING_TABLE in comms node although it passes
+@pytest.mark.parametrize("amount_of_nodes", [1])
+def test_locate_unregistered_peer_id(comms_nodes):
+    comms_node = comms_nodes[1]
+
+    # attempt to locate an unregistered peer
+    assert comms_node.client._get_peer_id(generate_address()) is ""
+
+
+# FIXME: comms node prints strange ServerUnaryCall message
+@pytest.mark.parametrize("amount_of_nodes", [1])
+def test_has_subscriber_self(comms_nodes):
+    comms_node = comms_nodes[1]
+    client, address = comms_node.client, comms_node.address
+
+    client.subscribe_to(address)
+
+    # subscribe to self and check subscription
+    assert client.is_subscribed_to(address) is True
+
+
+@pytest.mark.parametrize("amount_of_nodes", [2])
+def test_has_subscriber(comms_nodes):
+    comms_node_1, comms_node_2 = comms_nodes[1], comms_nodes[2]
+    client_1, address_1 = comms_node_1.client, comms_node_1.address
+    client_2, address_2 = comms_node_2.client, comms_node_2.address
+
+    # FIXME: nodes shouldn't have to subscribe to themselves for this to work
+    client_1.subscribe_to(address_1)
+    client_2.subscribe_to(address_2)
+
+    # subscribe from node 1 to 2
+    client_1.subscribe_to(address_2)
+
+    # check subscriptions
+    assert client_1.is_subscribed_to(address_1) is True
+    assert client_1.is_subscribed_to(address_2) is True
+    assert client_2.is_subscribed_to(address_1) is False
+    assert client_2.is_subscribed_to(address_2) is True
+
+
+@pytest.mark.parametrize("amount_of_nodes", [2])
+def test_two_clients_cross_subscription(comms_nodes):
+    comms_node_1, comms_node_2 = comms_nodes[1], comms_nodes[2]
+    client_1, address_1 = comms_node_1.client, comms_node_1.address
+    client_2, address_2 = comms_node_2.client, comms_node_2.address
+
+    # FIXME: nodes shouldn't have to subscribe to themselves for this to work
+    client_1.subscribe_to(address_1)
+    client_2.subscribe_to(address_2)
+
+    # subscribe both nodes to each other
+    client_1.subscribe_to(address_2)
+    client_2.subscribe_to(address_1)
+
+    # check subscriptions
+    assert client_1.is_subscribed_to(address_1) is True
+    assert client_1.is_subscribed_to(address_2) is True
+    assert client_2.is_subscribed_to(address_1) is True
+    assert client_2.is_subscribed_to(address_2) is True
+
+
+@pytest.mark.parametrize("amount_of_nodes", [2])
+def test_two_clients_cross_messaging_same_topic(comms_nodes):
+    comms_node_1, comms_node_2 = comms_nodes[1], comms_nodes[2]
+    client_1, address_1 = comms_node_1.client, comms_node_1.address
+    client_2, address_2 = comms_node_2.client, comms_node_2.address
+
+    # FIXME: nodes shouldn't have to subscribe to themselves for this to work
+    client_1.subscribe_to(address_1)
+    client_2.subscribe_to(address_2)
+
+    # subscribe both nodes to each other
+    _, sub_1_to_2 = client_1.subscribe_to(address_2)
+    _, sub_2_to_1 = client_2.subscribe_to(address_1)
+
+    payload_1 = "hello from 1"
+    payload_2 = "hello from 2"
+
+    client_1.send_message(payload_1, address_2)
+    client_2.send_message(payload_2, address_1)
+
+    for resp in sub_1_to_2:
+        received_message = notification_to_payload(resp)
+        assert received_message == payload_1
+        break  # only 1 message is expected
+
+    for resp in sub_2_to_1:
+        received_message = notification_to_payload(resp)
+        assert received_message == payload_2
+        break  # only 1 message is expected
