666c8e6dad34b7988f223c32f8f0d328ab82776c
Marcos Martinez <mmartinezciompi@gmail.com>
2020-12-30 09:07:50 -0300

    reorderer test

--- raiden/tests/integration/network/transport/rif_comms/test_client.py
+++ raiden/tests/integration/network/transport/rif_comms/test_client.py
@@ -171,32 +171,6 @@ def test_send_message_invalid(comms_clients):
     client.send_message("echo", generate_address())
 
 
-@pytest.mark.parametrize("nodes_to_clients", [{"A": 1, "B": 1}, {"A": 2}])
-def test_send_message_subscription(comms_clients):
-    client_1 = comms_clients[1]
-    client_2 = comms_clients[2]
-
-    # node 2 must listen to its own topic
-    _, sub = client_2.subscribe_to(client_2.rsk_address.address)
-
-    # message should be successfully sent, no subscription needed
-    payload = "marco"
-    client_1.send_message(payload, client_2.rsk_address.address)
-    for resp in sub:
-        received_message = notification_to_payload(resp)
-        assert received_message == payload
-        break  # only 1 message is expected
-
-    # send message with subscription
-    client_1.subscribe_to(client_2.rsk_address.address)
-    payload = "polo"
-    client_1.send_message(payload, client_2.rsk_address.address)
-    for resp in sub:
-        received_message = notification_to_payload(resp)
-        assert received_message == payload
-        break  # only 1 message is expected
-
-
 @pytest.mark.parametrize("nodes_to_clients", [{"A": 1}])
 def test_send_message_self(comms_clients):
     client = comms_clients[1]
@@ -237,6 +211,84 @@ def test_send_message_peers(comms_clients):
         received_message = notification_to_payload(resp)
         assert received_message == payload_1
         break  # only 1 message is expected
+
+
+@pytest.mark.parametrize("nodes_to_clients", [{"A": 1, "B": 1}, {"A": 2}])
+def test_send_message_sender(comms_clients):
+    client_1 = comms_clients[1]
+    client_2 = comms_clients[2]
+
+    # subscribe both nodes to each other
+    _, sub_1 = client_1.subscribe_to(client_1.rsk_address.address)
+    _, sub_2 = client_2.subscribe_to(client_2.rsk_address.address)
+
+    # send messages and listen
+    client_1.send_message("ping", client_2.rsk_address.address)
+    client_2.send_message("pong", client_1.rsk_address.address)
+
+    for resp in sub_1:
+        sender = get_sender_from_notification(resp)
+        assert sender == to_canonical_address(client_2.rsk_address.address)
+        break  # only 1 message is expected
+
+    for resp in sub_2:
+        sender = get_sender_from_notification(resp)
+        assert sender == to_canonical_address(client_1.rsk_address.address)
+        break  # only 1 message is expected
+
+
+@pytest.mark.parametrize("nodes_to_clients", [{"A": 1, "B": 1}, {"A": 2}])
+def test_send_message_subscription(comms_clients):
+    client_1 = comms_clients[1]
+    client_2 = comms_clients[2]
+
+    # node 2 must listen to its own topic
+    _, sub = client_2.subscribe_to(client_2.rsk_address.address)
+
+    # message should be successfully sent, no subscription needed
+    payload = "marco"
+    client_1.send_message(payload, client_2.rsk_address.address)
+    for resp in sub:
+        received_message = notification_to_payload(resp)
+        assert received_message == payload
+        break  # only 1 message is expected
+
+    # send message with subscription
+    client_1.subscribe_to(client_2.rsk_address.address)
+    payload = "polo"
+    client_1.send_message(payload, client_2.rsk_address.address)
+    for resp in sub:
+        received_message = notification_to_payload(resp)
+        assert received_message == payload
+        break  # only 1 message is expected
+
+
+def test_send_message_shutdown():
+    # the comms_nodes fixture is not used in order to shut down nodes manually
+    nodes = []
+    try:
+        node_1 = CommsNode(CommsConfig(node_id="A", amount_of_clients=1, auto_connect=False))
+        nodes.append(node_1)
+        client_1, address_1 = node_1.clients[0], node_1.clients[0].rsk_address.address
+
+        node_2 = CommsNode(CommsConfig(node_id="B", amount_of_clients=1, auto_connect=False))
+        nodes.append(node_2)
+        client_2, address_2 = node_2.clients[0], node_2.clients[0].rsk_address.address
+
+        # connect clients
+        client_1.connect()
+        client_2.connect()
+
+        # shut down node 2
+        node_2.stop()
+
+        # send message
+        client_1.send_message("into the void", address_2)  # no exception should be raised
+    finally:
+        for node in nodes:
+            # because node 2 can be already stopped, we cannot call stop() indiscriminately
+            if not node._process.poll():  # if True, node is still running
+                node.stop()
 
 
 @pytest.mark.parametrize("nodes_to_clients", [{"A": 1}])
@@ -294,55 +346,3 @@ def test_unsubscribe_from_peers(comms_clients):
     client_2.unsubscribe_from(client_1.rsk_address.address)
     assert client_2._is_subscribed_to(client_1.rsk_address.address) is False
     assert client_1._is_subscribed_to(client_2.rsk_address.address) is False  # check operations are independent
-
-
-@pytest.mark.parametrize("nodes_to_clients", [{"A": 1, "B": 1}, {"A": 2}])
-def test_send_message_sender(comms_clients):
-    client_1 = comms_clients[1]
-    client_2 = comms_clients[2]
-
-    # subscribe both nodes to each other
-    _, sub_1 = client_1.subscribe_to(client_1.rsk_address.address)
-    _, sub_2 = client_2.subscribe_to(client_2.rsk_address.address)
-
-    # send messages and listen
-    client_1.send_message("ping", client_2.rsk_address.address)
-    client_2.send_message("pong", client_1.rsk_address.address)
-
-    for resp in sub_1:
-        sender = get_sender_from_notification(resp)
-        assert sender == to_canonical_address(client_2.rsk_address.address)
-        break  # only 1 message is expected
-
-    for resp in sub_2:
-        sender = get_sender_from_notification(resp)
-        assert sender == to_canonical_address(client_1.rsk_address.address)
-        break  # only 1 message is expected
-
-
-def test_send_message_shutdown():
-    # the comms_nodes fixture is not used in order to shut down nodes manually
-    nodes = []
-    try:
-        node_1 = CommsNode(CommsConfig(node_id="A", amount_of_clients=1, auto_connect=False))
-        nodes.append(node_1)
-        client_1, address_1 = node_1.clients[0], node_1.clients[0].rsk_address.address
-
-        node_2 = CommsNode(CommsConfig(node_id="B", amount_of_clients=1, auto_connect=False))
-        nodes.append(node_2)
-        client_2, address_2 = node_2.clients[0], node_2.clients[0].rsk_address.address
-
-        # connect clients
-        client_1.connect()
-        client_2.connect()
-
-        # shut down node 2
-        node_2.stop()
-
-        # send message
-        client_1.send_message("into the void", address_2)  # no exception should be raised
-    finally:
-        for node in nodes:
-            # because node 2 can be already stopped, we cannot call stop() indiscriminately
-            if not node._process.poll():  # if True, node is still running
-                node.stop()
