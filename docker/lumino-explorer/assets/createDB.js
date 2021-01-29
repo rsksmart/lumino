a57300644b483c8839e538c0c392b8d653853fd3
Jonathan <jonathan.franco.2103@gmail.com>
2021-01-29 15:55:07 -0300

    RLP-981 Fixing regtest after mainnet and testnet changes

---
+++ docker/lumino-explorer/assets/createDB.js
@@ -0,0 +1,2 @@
+newDatabase = db.getSiblingDB("lumino_explorer");
+newDatabase.event_job_metadata.insert({ _id: "1",  lastSyncBlockChannels:"0", lastSyncBlockTokens: "0", periodOfSchedule: "500", _class: "org.rif.lumino.explorer.models.documents.EventJobMetadata"});
--- docker/lumino-explorer/assets/db-setup.js
+++
@@ -1,5 +0,0 @@
-databaseName = "lumino_explorer";
-db = db.getSiblingDB(databaseName);
-db.event_job_metadata.insert({ _id: "1",  lastSyncBlockChannels:"0", lastSyncBlockTokens: "0", periodOfSchedule: "500", _class: "org.rif.lumino.explorer.models.documents.EventJobMetadata"});
-db.getCollectionNames();
-db.event_job_metadata.count();
